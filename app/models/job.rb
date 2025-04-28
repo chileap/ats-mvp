288002c3c4e4532edd55e3c842bdcc523ebd4a65
Chileap Chhin <chileapchhin@gmail.com>
2025-04-28 18:17:51 +0200

    add scope and seed data with test

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,29 @@ class Job < ApplicationRecord
   has_many :events, class_name: 'Job::Event', dependent: :destroy
   has_many :applications, dependent: :destroy
   has_many :candidates, through: :applications
+
+  scope :activated, -> {
+    # Find jobs where the most recent event is an activation event
+    where(id: Job::Event.select(:job_id)
+      .where(type: 'Job::Event::Activated')
+      .where('created_at = (
+        SELECT MAX(created_at)
+        FROM job_events e2
+        WHERE e2.job_id = job_events.job_id
+      )'))
+  }
+
+  def status
+    last_event = events.order(created_at: :desc).first
+    return 'deactivated' if last_event.nil?
+
+    case last_event.type
+    when 'Job::Event::Activated'
+      'activated'
+    when 'Job::Event::Deactivated'
+      'deactivated'
+    else
+      'deactivated'
+    end
+  end
 end
